e25981cce0846bfc5fcefcc3d272a22f414bd3ca
taskylizard <75871323+taskylizard@users.noreply.github.com>
2026-01-11 13:38:47 +0100

    WIP

--- apps/www/.vitepress/theme/components/Index.vue
+++ apps/www/.vitepress/theme/components/Index.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="page-root font-rethink">
     <div class="page-canvas">
-      <div class="header">
+      <header class="header">
         <div class="logo" />
-
         <div class="brand-name">flora</div>
-
         <div class="project-tagline">a uwu.network project</div>
-      </div>
+      </header>
 
-      <div class="description">
+      <section class="description">
         flora is a fast runtime that lets you write discord bots for your
         servers, with a rich TypeScript SDK, without worrying about running
         infrastructure.
@@ -17,67 +15,44 @@
         You deploy scripts on a per-guild basis through a single bot, handled
         through the V8 JavaScript engine using V8 Isolates.
         <br /><br />
-      This is still taking shape, and will have a proper release soon. To stay
-      up to date on flora's progress, check out its repository:
-      <a class="underline" href="https://github.com/uwu/flora">
-        https://github.com/uwu/flora
-      </a>
-      <br /><br />
-      Start here:
-      <a class="underline" href="/sdk">
-        SDK docs
-      </a>
-      ·
-      <a class="underline" href="/runtime">
-        Runtime docs
-      </a>
-      ·
-      <a class="underline" href="/cli">
-        CLI docs
-      </a>
-      ·
-      <a class="underline" href="/examples">
-        Examples
-      </a>
+        This is still taking shape, and will have a proper release soon. To stay
+        up to date on flora's progress, check out its repository:
+        <a class="underline" href="https://github.com/uwu/flora">
+          https://github.com/uwu/flora
+        </a>
+        <br /><br />
+        Start here:
+        <a class="underline" href="/sdk">SDK docs</a>
+        ·
+        <a class="underline" href="/runtime">Runtime docs</a>
+        ·
+        <a class="underline" href="/cli">CLI docs</a>
+        ·
+        <a class="underline" href="/examples">Examples</a>
+      </section>
     </div>
 
-      <div class="background-gif" />
-    </div>
+    <footer class="footer-bg" />
   </div>
 </template>
 
 <style>
 .page-root {
-  background-color: #a4b793 !important;
-  box-sizing: border-box;
   min-height: 100vh;
-  width: 100%;
-  contain: content;
-  font-synthesis: none;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  position: relative;
+  display: flex;
+  flex-direction: column;
+  background-color: #a4b793;
 }
 
 .page-canvas {
-  position: relative;
-  width: 100%;
   max-width: 1440px;
-  height: 1024px;
   margin: 0 auto;
-  transform: scale(var(--canvas-scale, 1));
-  transform-origin: top center;
+  width: 100%;
 }
 
 .header {
-  box-sizing: border-box;
-  contain: content;
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 1440px;
+  position: relative;
   height: 202px;
-  z-index: 2;
 }
 
 .logo {
@@ -87,111 +62,51 @@
   width: 200px;
   height: 200px;
   background-image: url("/flora-logo.png");
-  background-position: center;
   background-size: cover;
-  box-sizing: border-box;
+  background-position: center;
 }
 
 .brand-name {
   position: absolute;
-  top: 0;
-  left: 0;
-  translate: 163px 68px;
+  left: 163px;
+  top: 68px;
   color: #e1ecd9;
   font-size: 52px;
   line-height: 58px;
-  letter-spacing: -0.01em;
-  width: fit-content;
-  height: fit-content;
-  box-sizing: border-box;
 }
 
 .project-tagline {
   position: absolute;
-  top: 0;
-  left: 0;
-  translate: 982px 68px;
+  right: 40px;
+  top: 68px;
   color: #e1ecd9;
   font-size: 34px;
   line-height: 52px;
-  letter-spacing: -0.01em;
-  width: fit-content;
-  height: fit-content;
-  box-sizing: border-box;
 }
 
 .description {
-  position: absolute;
-  top: 0;
-  left: 0;
-  translate: 54px 200px;
-  width: 1323px;
-  height: 586px;
+  max-width: 980px;
+  margin-top: 32px;
+  padding: 0 54px;
   color: #e1ecd9;
-  font-size: 34px;
-  line-height: 52px;
-  letter-spacing: -0.01em;
-  font-variation-settings: "wght" 410;
-  font-weight: 410;
-  text-wrap: pretty;
-  box-sizing: border-box;
-  z-index: 2;
+  font-size: 20px;
+  line-height: 32px;
+  letter-spacing: -0.005em;
 }
 
-.background-gif {
-  position: absolute;
-  top: 0;
-  left: 0;
-  translate: 0 382px;
-  width: 1440px;
-  height: 642px;
+.footer-bg {
+  margin-top: auto;
+  width: 100%;
+  height: 420px;
   background-image: url("/flora-footer.gif");
-  background-position: center;
   background-size: cover;
-  box-sizing: border-box;
-  z-index: 1;
+  background-position: bottom center;
+  background-repeat: no-repeat;
   pointer-events: none;
 }
 
-@media (max-width: 1440px) {
-  .header {
-    width: 100%;
-  }
-
-  .project-tagline {
-    left: auto;
-    right: 40px;
-    translate: 0 68px;
-  }
-
-  .description {
-    width: calc(100% - 108px);
-  }
-
-  .background-gif {
-    width: 100%;
-  }
-}
-
-@media (min-width: 1200px) {
-  .page-root {
-    --canvas-scale: 0.92;
-  }
-
-  .background-gif {
-    left: 50%;
-    width: calc(100vw / var(--canvas-scale, 1));
-    translate: -50% 470px;
-  }
-}
-
 @media (max-width: 900px) {
-  .page-canvas {
-    height: 900px;
-  }
-
   .header {
-    width: 100%;
     height: 140px;
   }
 
@@ -201,61 +116,37 @@
   }
 
   .brand-name {
-    translate: 96px 32px;
+    left: 96px;
+    top: 32px;
     font-size: 36px;
-    line-height: 40px;
   }
 
   .project-tagline {
-    translate: 96px 74px;
-    font-size: 18px;
-    line-height: 24px;
-    left: 0;
+    left: 96px;
+    top: 74px;
     right: auto;
-    max-width: calc(100% - 120px);
-  }
-
-  .description {
-    translate: 20px 150px;
-    width: calc(100% - 40px);
-    height: auto;
     font-size: 18px;
-    line-height: 28px;
-  }
-
-  .background-gif {
-    translate: 0 520px;
-    width: 100%;
-    height: 360px;
-    background-position: bottom center;
-  }
-}
-
-@media (max-width: 520px) {
-  .page-canvas {
-    height: 820px;
-  }
-
-  .brand-name {
-    translate: 88px 28px;
-    font-size: 32px;
-  }
-
-  .project-tagline {
-    translate: 88px 68px;
-    font-size: 16px;
-    max-width: calc(100% - 104px);
   }
 
   .description {
-    translate: 16px 140px;
-    width: calc(100% - 32px);
+    padding: 0 20px;
     font-size: 16px;
     line-height: 26px;
   }
 
-  .background-gif {
-    translate: 0 500px;
+  .footer-bg {
+    height: 360px;
+  }
+}
+
+@media (max-width: 520px) {
+  .description {
+    padding: 0 16px;
+    font-size: 15px;
+    line-height: 24px;
+  }
+
+  .footer-bg {
     height: 320px;
   }
 }
